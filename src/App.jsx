7161402ff67df6bad894703fb1b2b890795f1db7
Suxrob <orifovsuxrov@gmail.com>
2024-03-08 22:42:15 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+const App = () => {
+  return (
+    <div className="w-[100%] h-screen bg-gradient-to-l from-blue-500 to-green-500">
+      <div>
+        <button
+          onClick={() => alert("bellingham")}
+          className="rounded-md bg-red-500 m-5 p-5 text-3xl font-[500] text-white"
+        >
+          1
+        </button>
+        <button
+          onClick={() => alert("o'qiga jinni")}
+          className="rounded-md bg-red-500 m-5 p-5 text-3xl font-[500] text-white"
+        >
+          2
+        </button>
+        <button
+          onClick={() => alert("kassoy")}
+          className="rounded-md bg-red-500 m-5 p-5 text-3xl font-[500] text-white"
+        >
+          3
+        </button>
+        <button
+          onClick={() => alert("teshu")}
+          className="rounded-md bg-red-500 m-5 p-5 text-3xl font-[500] text-white"
+        >
+          4
+        </button>
+        <button
+          onClick={() => alert("zamen")}
+          className="rounded-md bg-red-500 m-5 p-5 text-3xl font-[500] text-white"
+        >
+          5
+        </button>
+        <button
+          onClick={() => alert("messi or ronaldo?")}
+          className="rounded-md bg-red-500 m-5 p-5 text-3xl font-[500] text-white"
+        >
+          6
+        </button>
+        <button
+          onClick={() => alert("hujumchi")}
+          className="rounded-md bg-red-500 m-5 p-5 text-3xl font-[500] text-white"
+        >
+          7
+        </button>
+        <button
+          onClick={() => alert("teshu varatis")}
+          className="rounded-md bg-red-500 m-5 p-5 text-3xl font-[500] text-white"
+        >
+          8
+        </button>
+        <button
+          onClick={() => alert("bot")}
+          className="rounded-md bg-red-500 m-5 p-5 text-3xl font-[500] text-white"
+        >
+          9
+        </button>
+        <button
+          onClick={() => alert("football nimaligini bilmidi")}
+          className="rounded-md bg-red-500 m-5 p-5 text-3xl font-[500] text-white"
+        >
+          10
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
